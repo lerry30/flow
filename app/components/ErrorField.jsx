80b6a2f493bb19ca88efdf4579991644c7e9a5ee
lerry <johnlerryramossamson@gmail.com>
2024-11-21 16:11:43 +0800

    Updated component

--- app/components/ErrorField.jsx
+++ app/components/ErrorField.jsx
@@ -2,7 +2,7 @@ import { Text } from 'react-native';
 
 const ErrorField = ({ error }) => {
     return (
-        <Text className="w-full text-red-600 font-pregular">{error}</Text>
+        <Text className={`w-full text-red-600 font-pregular ${error?'block':'hidden'}`}>{error}</Text>
     )
 }
 
